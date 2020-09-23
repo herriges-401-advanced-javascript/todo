b0cdfd1e0b12bd974efb59936c9d5bbe0eb5ae09
Matt Herriges <herrigesmt@gmail.com>
2020-09-22 23:49:22 -0700

    got started on hooks and posting to/getting from api

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,47 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
+import Navbar from 'react-bootstrap/Navbar'
 
 import './todo.scss';
+import axios from 'axios';
 
-const ToDo = (props) => {
+const api = 'http://localhost:3001/todo'
+const ToDo = () => {
   
   const [list, setList] = useState([]);
 
   const _addItem = (item) => {
+    console.log('item', item)
     item._id = Math.random();
     item.complete = false;
+    if(!item.difficulty){
+      item.difficulty = 1;
+    }
     setList([...list, item]);
+    axios.post(api, {
+      text : item.text,
+      assignee : item.assignee,
+      difficulty : item.difficulty,
+      complete: item.complete,
+      id: item._id,
+    })
   }
 
-  const _toggleComplete = (id) => {
+  const _toggleComplete = async (id) => {
     let item = list.filter(i => i._id === id)[0] || {};
 
     if(item._id) {
       item.complete = !item.complete;
       let newList = list.map(listItem => listItem._id === item._id ? item : listItem);
-      setList(list);
+      setList(newList);
+      
     }
   }
 
-  const componentDidMount = () => {
-    let newList = [
-      { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
-      { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
-      { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
-      { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
-      { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
-    ];
-    setList(newList);
-  }
-
-  useEffect(() => {
-    document.title = `To Do List: ${list.length}`;
-  })
+  useEffect(async () => {
+    const response = await axios.get(api)
+    console.log('api data', response.data);
+    setList(response.data || []);
+  }, [])
+  
 
   return (
     <>
         <header>
+          
           <h2>
           There are {list.filter(item => !item.complete).length} Items To Complete
           </h2>
@@ -63,74 +71,5 @@ const ToDo = (props) => {
       </>
   )
 }
-/*
-class ToDo extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
-
-  addItem = (item) => {
-    item._id = Math.random();
-    item.complete = false;
-    this.setState({ list: [...this.state.list, item]});
-  };
-
-  toggleComplete = id => {
-
-    let item = this.state.list.filter(i => i._id === id)[0] || {};
-
-    if (item._id) {
-      item.complete = !item.complete;
-      let list = this.state.list.map(listItem => listItem._id === item._id ? item : listItem);
-      this.setState({list});
-    }
-
-  };
-
-  componentDidMount() {
-    let list = [
-      { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
-      { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
-      { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
-      { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
-      { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
-    ];
-
-    this.setState({list});
-  }
-
-  render() {
-    return (
-      <>
-        <header>
-          <h2>
-          There are {this.state.list.filter(item => !item.complete).length} Items To Complete
-          </h2>
-        </header>
-
-        <section className="todo">
-
-          <div>
-            <TodoForm handleSubmit={this.addItem} />
-          </div>
-
-          <div>
-            <TodoList
-              list={this.state.list}
-              handleComplete={this.toggleComplete}
-            />
-          </div>
-        </section>
-      </>
-    );
-  }
-}
-
-
-*/
 
 export default ToDo;
